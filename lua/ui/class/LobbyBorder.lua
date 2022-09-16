a3a0d89bf30b3d1607ccc765e3b7482040d766bc
Alexander <84857900+4z0t@users.noreply.github.com>
2022-09-16 13:40:53 +0300

    init LobbyBorder

---
+++ lua/ui/class/LobbyBorder.lua
@@ -0,0 +1,23 @@
+local Group = import("/lua/maui/group.lua").Group
+
+
+
+LobbyBorder = Class(Group)
+{
+    __init = function(self, parent)
+        Group.__init(self, parent)
+
+        
+    end,
+
+
+    __post_init = function (self)
+        self:_Layout()
+    end,
+
+    _Layout = function (self)
+        
+    end
+
+
+}
